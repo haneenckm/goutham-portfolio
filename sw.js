a626d49f92994dc8d4124cd30a77caf3e1ada874
haneenckm <haneenckm03@gmail.com>
2026-08-19 02:31:02 +0530

    Add Progressive Web App (PWA) support with Add to Home Screen for iOS and Android

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'goutham-portfolio-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/goutham-hero.jpg',
+  '/blog-thumbnail.jpg',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch((err) => {
+        console.log('Cache addAll non-critical error:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse));
+          }
+        }).catch(() => {});
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
